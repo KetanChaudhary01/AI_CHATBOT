f0a4191590744e6e5ca7ac634b2b7849b6ec3b98
Ketan Chaudhary <ketanchaudhary0001@gmail.com>
2026-06-10 14:50:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import google.generativeai as genai 
+st.set_page_config(page_title="ketan_chatbot", page_icon="🤖")
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+model=genai.GenerativeModel("gemini-flash-latest")
+st.title("Ketan's Chatbot")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+if prompt := st.chat_input("You: "):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+    response = model.generate_content(prompt)
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
+    with st.chat_message("assistant"):
+        st.write(response.text)
